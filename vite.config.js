e1287ad8f45ec2bf2c6a417c946e4e58f2679d6b
Victoria the Magpie <github@themagpie.anonaddy.com>
2025-05-23 14:46:21 -0400

    Config changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://infomagpie.blog/'
+  vite: {
+    server: {
+      fs: {
+        strict: false
+      }
+    }
+  }
 });
