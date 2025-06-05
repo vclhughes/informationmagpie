8bdd9301f418a5901b95e223d665af7904e75df2
Victoria the Magpie <github@themagpie.anonaddy.com>
2025-06-05 14:32:11 -0400

    Updating RSS Feed, changing color palette

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: 'Info Magpie | Blog',
+    description: 'A little bit of this and that',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
